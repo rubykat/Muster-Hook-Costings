3fe2f8f5080751316a028ac731a135817ff8349c
Kathryn Andersen <perlkat@katspace.org>
2017-12-08 13:11:34 +1100

    tweaking the overheads again

--- lib/Muster/Hook/Costings.pm
+++ lib/Muster/Hook/Costings.pm
@@ -474,13 +474,13 @@ sub calculate_overheads {
     # So I'll just make it 10%.
     my $overheads = $bare_cost * 0.10;
 
+    # Add another $2 for Promoted Listings (at a budget of US$1 per day)
+    # (see the money-etsy page for the calculations)
+    $overheads += 2;
+    
     # And now Etsy are charging GST on their fees
     $overheads += $overheads * 0.1;
 
-    # Add another 4% for random other overheads, such as handling.
-    $bare_cost += $overheads;
-    $overheads += ($bare_cost * 0.04);
-    
     # I'm not including Paypal here -- that's for if I'm not selling through Etsy.
     # (Paypal fees: 3.5% plus 30c per transaction?)
     # GST is not included because I don't have to pay GST because I'm not making $75,000
